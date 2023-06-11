301ee1d701d9db92fcb47b28142b0162a36f01f2
csisko <w22b003@technikum-wien.at>
2023-06-11 15:50:52 +0200

    Bugfixing

--- src/main/java/at/technikum/MessageController.java
+++ src/main/java/at/technikum/MessageController.java
@@ -7,15 +7,17 @@ import org.springframework.web.bind.annotation.*;
 public class MessageController {
     static String defaultMessage = "Everything operates as expected";
     static String currentMessage = defaultMessage;
-
+    static String testMessage;
     @GetMapping("/api/message")
     public String getMessage() {
         return currentMessage;
     }
 
     @GetMapping("/api/message/set")
-    public String setMessage(@RequestParam String m){
+    public static String setMessage(@RequestParam String m) {
+        testMessage = m;
         currentMessage = m;
+
         return "ok";
     }
 
--- src/test/java/at/technikum/MessageControllerTest.java
+++ src/test/java/at/technikum/MessageControllerTest.java
@@ -3,13 +3,20 @@ package at.technikum;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import at.technikum.MessageController;
 
 class MessageControllerTest {
 
-        @Test
-    void defaultMessage(){
+    @Test
+    void defaultMessage() {
         String ABC = "Everything operates as expected";
 
-        assertEquals(ABC,MessageController.currentMessage);
+        assertEquals(ABC, MessageController.currentMessage);
     }
+
+    @Test
+    void testSetMessage() {
+        assertEquals(MessageController.currentMessage, MessageController.setMessage(MessageController.currentMessage));
+    }
+
 }
